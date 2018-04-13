7556ae5e93baf5ab0931f969b745128fae05b57d
christofergiron <31929451+christofergiron@users.noreply.github.com>
2018-04-13 09:52:01 -0600

    correcciones usuarios

--- database/migrations/2018_03_19_233607_create_contra_orden_capturas_table.php
+++ database/migrations/2018_03_19_233607_create_contra_orden_capturas_table.php
@@ -17,9 +17,11 @@ class CreateContraOrdenCapturasTable extends Migration
             $table->increments('id');
             //si no es solicitud ocuparia los datos que solicitud tiene
             $table->integer('id_orden');
-            $table->string('estado');
-            //porque se emite esta cosa
+            $table->DateTime('fecha_creacion');
+            $table->integer('id_expediente');
             $table->string('razon');
+            $table->integer('id_juez')->nullable();
+            $table->integer('id_fiscal')->nullable();
             $table->string('descripcion')->nullable();
             $table->boolean('deleted')->default(false);
             $table->timestamps();
